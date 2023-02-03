4718318f85ffca86ef5a67f5eec26ad091033509
reactmatz <matheusnasc.contato@gmail.com>
2023-02-02 22:28:21 -0300

    Add: readme concept photo-app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AppClima
 Aplicativo que retorna a temperatura de uma regiao no mundo em tempo real, o background se modifica de acordo com o local consultado, e por fim, retorna a bandeira do pais consultado. 
+
+####### First concept:
+
+<img src="img/foto.png" width= 600px > 
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 // 99292d8c04554b4fdd0d7f6290bcd9b0
 
-const apiKey = "";
-const apiCountryURL = "https://countryflagsapi.com/png/";
-const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+// const apiKey = "";
+// const apiCountryURL = "https://countryflagsapi.com/png/";
+// const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
-const cityInput = document.querySelector("#city-input");
-const searchBtn = document.querySelector("#search");
+// const cityInput = document.querySelector("#city-input");
+// const searchBtn = document.querySelector("#search");
 
-const cityElement = document.querySelector("#city");
-const tempElement = document.querySelector("#temperature span");
-const descElement = document.querySelector("#description");
-const weatherIconElement = document.querySelector("#weather-icon");
-const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
-const windElement = document.querySelector("#wind span");
+// const cityElement = document.querySelector("#city");
+// const tempElement = document.querySelector("#temperature span");
+// const descElement = document.querySelector("#description");
+// const weatherIconElement = document.querySelector("#weather-icon");
+// const countryElement = document.querySelector("#country");
+// const umidityElement = document.querySelector("#umidity span");
+// const windElement = document.querySelector("#wind span");
 
-const weatherContainer = document.querySelector("#weather-data");
+// const weatherContainer = document.querySelector("#weather-data");
 
-const errorMessageContainer = document.querySelector("#error-message");
-const loader = document.querySelector("#loader");
+// const errorMessageContainer = document.querySelector("#error-message");
+// const loader = document.querySelector("#loader");
 
-const suggestionContainer = document.querySelector("#suggestions");
-const suggestionButtons = document.querySelectorAll("#suggestions button");
+// const suggestionContainer = document.querySelector("#suggestions");
+// const suggestionButtons = document.querySelectorAll("#suggestions button");
 
-// Loader
-const toggleLoader = () => {
-  loader.classList.toggle("hide");
-};
+// // Loader
+// const toggleLoader = () => {
+//   loader.classList.toggle("hide");
+// };
